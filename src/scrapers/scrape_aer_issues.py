d59b37e44953f2dabc13d9dd768ddf38d4e64484
lachlan <lachlan.deer@uzh.ch>
2018-02-02 20:04:24 +0100

    add header for python script to get AER issues

---
+++ src/scrapers/scrape_aer_issues.py
@@ -0,0 +1,20 @@
+"""
+scrape_issues.py
+
+contributor: @ lachlandeer
+
+This script scrapes all the issue links from the AER main website,
+saving them as a csv file
+
+Inputs:
+    web_url: the current AER website
+
+Outputs:
+    aer_isses.csv : a csv file with a list of links to AER issues linked
+                    from the AER homepage
+
+Depends on:
+    NULL
+"""
+
+print('hello world')
